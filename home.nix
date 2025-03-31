257b573ea909c1689bdc74d839549aaf7e8dc562
isabella <isabellaoliveiradev@gmail.com>
2025-03-31 15:15:08 -0300

    refactor: removes default commentaries and unused code

--- home.nix
+++ home.nix
@@ -39,42 +39,11 @@
     '')
   ];
   
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-  #programs.neovim = 
-  #let
-  #  toLua = str: "lua << EOF\n${str}\nEOF\n";
-  #  toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-  #in
-  #{
-  #  enable = true;
-  #  defaultEditor = true; 
-  #  extraLuaConfig = ''
-  #    ${builtins.readFile ./nvim/options.lua}
-  #  '';
-  #  plugins = with pkgs.vimPlugins; [
-  #    { plugin = telescope-nvim; config = toLuaFile ./nvim/plugin/telescope.lua;}
-  #    { plugin = lsp-zero-nvim; config = toLuaFile ./nvim/plugin/lsp.lua;}
-  #  ];
-  #};
 
   #dont change
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 }
