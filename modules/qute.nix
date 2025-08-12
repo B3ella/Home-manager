5fef3d0d4c49c1cc9ead0a58489309570ae3b874
b3ella <goliveirapk@gmail.com>
2025-08-12 13:40:32 -0300

    maintanance: remove unused commands

--- modules/qute.nix
+++ modules/qute.nix
@@ -5,8 +5,6 @@
     enable = true;
     keyBindings = {
       normal = {
-        "<Ctrl+p>" = "open -t https://keepersecurity.com/vault/";
-        "<Ctrl+d>" = "open -t https://drive.google.com/drive/u/0/home";
         "<Ctrl+w>" = "open -t https://web.whatsapp.com/";
         "<Ctrl+e>" = "open -t https://ea.uniceub.br/Sistema/Acesso/Login";
         "<Ctrl+g>" = "open -t https://github.com/B3ella";
