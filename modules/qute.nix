846d7e2f23f49e4d7f8c2c67f26c1ceae72cd071
b3ella <goliveirapk@gmail.com>
2025-09-08 09:21:59 -0300

    feat: jess github

--- modules/qute.nix
+++ modules/qute.nix
@@ -9,6 +9,7 @@
         "<Ctrl+e>" = "open -t https://ea.uniceub.br/Sistema/Acesso/Login";
         "<Ctrl+g>" = "open -t https://github.com/B3ella";
         "<Ctrl+m>" = "open -t https://mail.google.com/mail/u/0/#inbox";
+        "<Ctrl+j>" = "open -t https://github.com/gathddu";
       };
     };
   };
